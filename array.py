4508299ef0d41644e5a54ca90a852f7f838e4bfd
Dillon Huff <dillonhuff@gmail.com>
2019-01-02 14:04:00 -0800

    Creating systolic array loading code

--- array.py
+++ array.py
@@ -74,19 +74,113 @@ class Tile:
 class SysArray:
     def __init__(self, nrows, ncols):
         self.row_fifos = []
-        for i in range(nrows):
-            self.row_fifos = deque()
+        self.nrows = nrows
+        self.ncols = ncols
 
-        for j in range(ncols):
-            self.col_fifos = deque()
+        self.col_fifos = []
+        self.row_fifos = []
+
+        self.fifo_depth = max(nrows, ncols) + 1
+
+        for i in range(self.nrows):
+            zeros = []
+            for j in range(self.fifo_depth):
+                zeros.append(0)
+
+            self.row_fifos.append(deque(zeros))
+
+        for j in range(self.ncols):
+            zeros = []
+            for j in range(self.fifo_depth):
+                zeros.append(0)
+            
+            self.col_fifos.append(deque(zeros))
 
         self.tile_rows = {}
-        for i in range(nrows):
+        for i in range(self.nrows):
             tile_row = {}
-            for j in range(ncols):
+            for j in range(self.ncols):
                 tile_row[j] = Tile()
 
             self.tile_rows[i] = tile_row
 
-sa = SysArray(5, 5)
-            
+
+    def get_tile(self, i, j):
+        return self.tile_rows[i][j]
+
+    def read_col(self, i):
+        print(self.col_fifos[i])
+        val = self.col_fifos[i].pop()
+        self.col_fifos[i].appendleft(0)
+
+        return val
+
+    def read_row(self, i):
+        print(self.row_fifos[i])
+        val = self.row_fifos[i].pop()
+        self.row_fifos[i].appendleft(0)
+
+        return val
+        
+    def update(self):
+        for i in range(self.nrows):
+            for j in range(self.ncols):
+                if i == 0:
+                    top = self.read_col(j)
+                else:
+                    top = self.get_tile(i - 1, j).down
+
+                if j == 0:
+                    left = self.read_row(i)
+                else:
+                    left = self.get_tile(i, j - 1).right
+
+
+                self.get_tile(i, j).update_val(top, left)
+
+
+        for i in range(self.nrows):
+            for j in range(self.ncols):
+                if i == 0:
+                    top = self.read_col(j)
+                else:
+                    top = self.get_tile(i - 1, j).down
+
+                if j == 0:
+                    left = self.read_row(i)
+                else:
+                    left = self.get_tile(i, j - 1).right
+
+
+                self.get_tile(i, j).update_outputs(top, left)
+                
+    def __str__(self):
+        s = ''
+        return s
+
+sa = SysArray(1, 2)
+
+print(sa)
+
+sa.update()
+
+print(sa)
+
+a = Matrix(1, 2)
+a.set(0, 0, 1)
+a.set(0, 1, 2)
+
+print(a)
+
+
+b = Matrix(2, 2)
+b.set(0, 0, 4)
+b.set(0, 1, 5)
+b.set(1, 0, 6)
+b.set(1, 1, 7)
+
+print(b)
+
+print('Correct product')
+prod = mul(a, b)
+print(prod)
